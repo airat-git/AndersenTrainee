d5aa268df06c0a211fc7a7600e6ebe700b79dfdc
airat-git <airat_f17@mail.ru>
2020-10-28 14:54:44 +0300

    my linkedlist implementation added (main methods only, very simple implementation)

---
+++ src/main/java/ru/javatrainee/study/MyLinkedListTest.java
@@ -0,0 +1,57 @@
+package ru.javatrainee.study;
+
+import ru.javatrainee.study.mylist.MyLinkedList;
+import java.util.Iterator;
+
+public class MyLinkedListTest {
+
+    public static void main(String[] args) {
+
+        MyLinkedList<String> myList = new MyLinkedList();
+        myList.remove("a");
+        System.out.println("Мой лист пустой:");
+        System.out.println(myList);
+        myList.add("A");
+        System.out.println("Мой лист после добавления одного элемента 'A':");
+        System.out.println(myList);
+        System.out.println(myList.size());
+        myList.remove("A");
+        System.out.println("Мой лист после удаления единственного элемента:");
+        System.out.println(myList);
+        myList.add("A");
+        myList.add("B");
+        myList.add("C");
+        myList.add("D");
+        myList.add("E");
+        myList.add("F");
+        System.out.println("Мой лист после добавления 6 элементов:");
+        System.out.println(myList);
+        myList.add("G");
+        myList.add("H");
+        myList.add("I");
+        System.out.println("Мой лист после добавления еще 3 элементов:");
+        System.out.println(myList);
+        System.out.println("6 элемент моего листа (по индексу):");
+        System.out.println(myList.get(6));
+        myList.remove(4);
+        System.out.println("Мой лист после удаления элемента по индексу '4':");
+        System.out.println(myList);
+        myList.remove("F");
+        System.out.println("Мой лист после удаления элемента 'F':");
+        System.out.println(myList);
+        myList.add("hi!");
+        System.out.println("Мой лист после добавления элемента 'hi':");
+        System.out.println(myList);
+        myList.poll();
+        System.out.println("Мой лист после метода poll():");
+        System.out.println(myList);
+        System.out.println("Итерация по элементам листа:");
+        Iterator iterator = myList.iterator();
+        int i = 1;
+        while (iterator.hasNext()){
+            System.out.printf("Элемент №%d:",i);
+            System.out.println(iterator.next());
+            i++;
+        }
+    }
+}
