42ae0f2aedc793a2c4ecaba0f01db5b0d2c974e4
airat-git <airat_f17@mail.ru>
2020-11-10 11:59:59 +0300

    some changes to multithreading implementation added

--- src/main/java/ru/javatrainee/study/multithreading/Main.java
+++ src/main/java/ru/javatrainee/study/multithreading/Main.java
@@ -10,13 +10,7 @@ public class Main {
 
         new Thread(()-> System.out.println("Thread name: " +
                 Thread.currentThread().getName()),"MyRunnableThread").start();
-        Callable myCallableThread = new Callable() {
-            @Override
-            public Object call() throws Exception {
-                return Thread.currentThread().getName();
-            }
-        };
-        FutureTask<String> futureTask = new FutureTask<String>(myCallableThread);
+        FutureTask<String> futureTask = new FutureTask<String>(new MyCallableThread());
         new Thread(futureTask,"Thread name: MyCallableThread").start();
         System.out.println(futureTask.get());
     }
